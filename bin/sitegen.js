da4ca9a2b2735a3ec6fc0b40af457f1d7f8b5324
Joe Ferner <joe@fernsroth.com>
2013-03-28 17:01:37 -0400

    progress

---
+++ bin/sitegen.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+'use strict';
+
+var optimist = require('optimist');
+var sitegen = require('../lib/');
+
+var argv = optimist
+  .usage('Usage: sitegen.js [options]')
+  .alias('help', 'h')
+  .alias('h', '?')
+  .argv;
+
+if (argv.help) {
+  optimist.showHelp();
+  process.exit(1);
+}
+
+process.on('uncaughtException', function(err) {
+  console.error('uncaughtException', err.stack || err);
+});
+
+sitegen(argv, function(err) {
+  if (err) {
+    console.error(err.stack);
+  }
+});
